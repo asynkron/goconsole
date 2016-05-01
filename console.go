ae95538c06662706d491b857df00030bdef9ae9f
rogeralsing <rogeralsing@gmail.com>
2016-05-01 14:51:52 +0200

    first

---
+++ console.go
@@ -0,0 +1,16 @@
+package console
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+func ReadLine() (string, error) {
+	text, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	text = strings.TrimRight(text, "\n\r")
+	return text, nil
+}
