d3fc2e33683e5c3fb3c8d1c6bdce5a195c1e739e
rogeralsing <rogeralsing@gmail.com>
2016-05-04 16:32:06 +0200

    console commands

--- console.go
+++ console.go
@@ -2,6 +2,7 @@ package console
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strings"
 )
@@ -13,4 +14,52 @@ func ReadLine() (string, error) {
 	}
 	text = strings.TrimRight(text, "\n\r")
 	return text, nil
+}
+
+type Console struct {
+	empty    func(string)
+	commands []*Command
+}
+
+type Command struct {
+	Name     string
+	Callback func(string)
+}
+
+func NewConsole(empty func(string)) *Console {
+	cons := &Console{
+		commands: make([]*Command, 0),
+		empty:    empty,
+	}
+	return cons
+}
+
+func (cons *Console) Command(name string, callback func(string)) {
+	command := &Command{
+		Name:     name,
+		Callback: callback,
+	}
+	cons.commands = append(cons.commands, command)
+}
+
+func (cons *Console) Run() {
+	for {
+		text, err := ReadLine()
+		if err != nil {
+			panic("Error reading console input")
+		}
+		parts := strings.Split(text, " ")
+		fmt.Println(parts)
+
+		for _, command := range cons.commands {
+			prefix := command.Name + " "
+			if strings.HasPrefix(text, prefix) {
+				parts := strings.Split(text," ")
+				command.Callback(parts[1])
+				break
+			}
+		}
+		cons.empty(text)
+
+	}
 }
